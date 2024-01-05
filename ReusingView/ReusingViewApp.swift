ef3b32298a84e0fbdb9f8cda2d0e2076c305176c
erwinagpasa <rootaccess000@gmail.com>
2024-01-05 19:28:23 +0300

    Initial Commit

---
+++ ReusingView/ReusingViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReusingViewApp.swift
+//  ReusingView
+//
+//  Created by Developer on 05/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReusingViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
