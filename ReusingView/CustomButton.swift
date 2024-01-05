4602e60df9c2442e45e63b9f5affeff066c0bccb
erwinagpasa <rootaccess000@gmail.com>
2024-01-05 19:45:02 +0300

    adding screens

---
+++ ReusingView/CustomButton.swift
@@ -0,0 +1,18 @@
+//
+//  CustomButton.swift
+//  ReusingView
+//
+//  Created by erwinagpasa on 05/01/2024.
+//
+
+import SwiftUI
+
+struct CustomButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomButton()
+}
